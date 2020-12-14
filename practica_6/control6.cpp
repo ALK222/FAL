182b7bc0ea874b3d4de5a05d268a0c1d45a50817
A.L.K <alejandrobarrachina.02@gmail.com>
2020-12-14 17:55:57 +0100

    aaaaaaaaa

--- practica_6/control6.cpp
+++ practica_6/control6.cpp
@@ -13,7 +13,7 @@ INDICARLO AQUI, EXPLICANDO TAMBI�N EL MOTIVO:
 
 // Si se comenta esta definici�n, el programa
 // autocomprobar� su funcionamiento comparando
-// la implementaci�n realizada con una
+// la implementaci�n realizada con una 
 // implementaci�n 'naif' del algoritmo
 #include <iostream>
 
@@ -22,7 +22,7 @@ INDICARLO AQUI, EXPLICANDO TAMBI�N EL MOTIVO:
 #ifndef DOM_JUDGE
 #include <ctime>
 #include <stdlib.h>
-#endif
+#endif 
 
 using namespace std;
 
@@ -40,6 +40,7 @@ descrito de manera clara y concisa.
 (2) An�lisis de casos:
 
 (2.1) Casos base
+n <= 10: num_sing = n;
 
 
 (2.2) Casos recursivos
@@ -54,30 +55,30 @@ a partir de los devueltos por la generalizaci�n.
 
 */
 
-t_num digito_mas_significativo_n(t_num n)
-{
-    while (n > 9)
-    {
-        n = n / 10;
-    }
-    return n;
+t_num digito_mas_significativo_n(t_num n) {
+	while (n > 9) {
+		n = n / 10;
+	}
+	return n;
 }
 
-t_num es_singular_n(t_num &n, t_num &total)
+void es_singular_n(t_num n, t_num& total)
 {
-    t_num msd = digito_mas_significativo_n(n);
-    if (n > 10 && msd != n % 10)
-    {
-        total += ((n / 100) + (n / 10));
-        n = n / 10;
-        es_singular_n(n, total);
-    }
-    else
-    {
-        return total;
-    }
-
-    /*if(n != 0)
+	if(n > 10)
+	{
+		if(n >= 100)
+		{
+			total += 19;
+			n -= 100;
+			es_singular_n(n, total);
+		}
+		else
+		{
+			total += ((n / 10));
+		}
+	}
+	
+	/*if(n != 0)
 	{
 		if(mas_sig != n % 10)
 		{
@@ -86,23 +87,46 @@ t_num es_singular_n(t_num &n, t_num &total)
 		n--;
 		es_singular_n(n, total, mas_sig);
 	}*/
-    return total;
+}
+void nnz_aux(t_num n, t_num& c, bool& ok)
+{
+
+	if (n <= 9)
+	{
+		c += max((t_num)0, n);
+		ok = true;
+	}
+	else{
+		
+		nnz_aux(n / 10, c, ok);
+
+		t_num msd = digito_mas_significativo_n(n);
+		c *= 9;
+
+		if(ok)
+		{
+			c += (n)% 10;
+		}
+
+		ok = ok && (n % 10 != msd);
+	}
 }
 
-t_num num_singulares_menoresque(t_num n)
-{
-    // Punto de entrada al algoritmo.
-    // A implementar
-    t_num num = 1;
-    t_num m = n;
-    t_num msd = digito_mas_significativo_n(n);
+t_num num_singulares_menoresque(t_num n) {
+	// Punto de entrada al algoritmo.
+	// A implementar
+	t_num num = 0;
+	t_num m = n;
+	t_num prueba = 0;
+	bool prueba1 = false;
+	t_num msd = digito_mas_significativo_n(n);
 
-    es_singular_n(m, num);
-    if (n == 0)
-    {
-        return 1;
-    }
-    return n + 1 - num;
+	nnz_aux(n, num, prueba1);
+	if( n == 0)
+	{
+		return 1;
+	}
+	return num;
 }
 
 /*
@@ -118,78 +142,64 @@ Determina justificadamente la complejidad del algoritmo
 */
 
 #ifndef DOM_JUDGE
-unsigned short digito_mas_significativo(t_num n)
-{
-    while (n > 9)
-    {
-        n = n / 10;
-    }
-    return (unsigned short)n;
+unsigned short digito_mas_significativo(t_num n) {
+	while (n > 9) {
+		n = n / 10;
+	}
+	return (unsigned short)n;
 }
-bool es_singular(t_num n)
-{
-    unsigned short msd = digito_mas_significativo(n);
-    bool loes = true;
-    while (n > 9 && loes)
-    {
-        loes = (n % 10 != msd);
-        n = n / 10;
-    }
-    return loes;
+bool es_singular(t_num n) {
+	unsigned short msd = digito_mas_significativo(n);
+	bool loes = true;
+	while (n > 9 && loes) {
+		loes = (n % 10 != msd);
+		n = n / 10;
+	}
+	return loes;
 }
 
-t_num num_singulares_menoresque_naif(t_num n)
-{
-    t_num num = 0;
-    for (t_num i = 0; i < n; i++)
-    {
-        if (es_singular(i))
-        {
-            num++;
-        }
-    }
-    return num;
+t_num num_singulares_menoresque_naif(t_num n) {
+	t_num num = 0;
+	for (t_num i = 0; i < n; i++) {
+		if (es_singular(i)) {
+			num++;
+		}
+	}
+	return num;
 }
 #endif
 
 #ifdef DOM_JUDGE
-bool ejecuta_caso()
-{
-    long long n;
-    cin >> n;
-    if (n == -1)
-    {
-        return false;
-    }
-    else
-    {
-        cout << num_singulares_menoresque(n) << endl;
-        return true;
-    }
+bool ejecuta_caso() {
+	long long n;
+	cin >> n;
+	if (n == -1) {
+		return false;
+	}
+	else {
+		cout << num_singulares_menoresque(n) << endl;
+		return true;
+	}
 }
 #endif
 
-int main()
-{
+int main() {
 
 #ifndef DOM_JUDGE
-    srand(time(NULL));
-    for (int i = 1; i < 1000; i++)
-    {
-        t_num n = (t_num)rand();
-        if (num_singulares_menoresque_naif(n) != num_singulares_menoresque(n))
-        {
-            cout << "NO FUNCIONA" << endl;
-            cout << n << ":" << num_singulares_menoresque_naif(n) << "..." << num_singulares_menoresque(n) << endl;
-            break;
-        }
-    }
-    cout << "OK" << endl;
-    system("pause");
+	srand(time(NULL));
+	for (int i = 1; i < 1000; i++) {
+		t_num  n = (t_num)rand();
+		if (num_singulares_menoresque_naif(n) != num_singulares_menoresque(n)) {
+			cout << "NO FUNCIONA" << endl;
+			cout << n << ":" << num_singulares_menoresque_naif(n) << "..." << num_singulares_menoresque(n) << endl;
+			break;
+		}
+	}
+	cout << "OK" << endl;
+	system("pause");
 #endif
 
 #ifdef DOM_JUDGE
-    while (ejecuta_caso())
-        ;
+	while (ejecuta_caso());
 #endif
 }
