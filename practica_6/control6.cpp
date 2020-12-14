1cbb347a5668bf90b5aa4d16e0078446a3d0f1d6
A.L.K <alejandrobarrachina.02@gmail.com>
2020-12-14 14:16:58 +0100

    me cago en todo

---
+++ practica_6/control6.cpp
@@ -0,0 +1,195 @@
+/*
+N� DE GRUPO: 09
+NOMBRE Y APELLIDOS DE LOS ESTUDIANTES QUE
+HAN REALIZADO LA PRACTICA: Alejandro Barrachina Argudo y Rodrigo Sosa Saez
+
+EN CASO DE QUE ALGUNO DE LOS MIEMBROS DEL
+GRUPO NO HAYA PARTICIPADO EN LA PRACTICA,
+INDICARLO AQUI, EXPLICANDO TAMBI�N EL MOTIVO:
+
+
+
+*/
+
+// Si se comenta esta definici�n, el programa
+// autocomprobar� su funcionamiento comparando
+// la implementaci�n realizada con una
+// implementaci�n 'naif' del algoritmo
+#include <iostream>
+
+#define DOM_JUDGE
+
+#ifndef DOM_JUDGE
+#include <ctime>
+#include <stdlib.h>
+#endif
+
+using namespace std;
+
+typedef unsigned long long t_num;
+
+/*
+(1) En caso de utilizar una generalizacion,
+determinaci�n de los par�metros de la generalizaci�n.
+Determina los par�metros de la generalizaci�n. Para cada par�metro
+indica: (i) su tipo; (ii) su nombre; (iii) si es un par�metro
+de entrada, de salida, o de entrada / salida; (iv) su prop�sito,
+descrito de manera clara y concisa.
+
+
+(2) An�lisis de casos:
+
+(2.1) Casos base
+
+
+(2.2) Casos recursivos
+
+(3) En caso de utilizar una generalizacion, definici�n por inmersi�n
+del algoritmo. Describe de manera clara y concisa c�mo se lleva a
+cabo el algoritmo, en qu� punto o puntos se invoca a la generalizaci�n,
+con qu� par�metros reales, y c�mo se genera el resultado
+a partir de los devueltos por la generalizaci�n.
+
+
+
+*/
+
+t_num digito_mas_significativo_n(t_num n)
+{
+    while (n > 9)
+    {
+        n = n / 10;
+    }
+    return n;
+}
+
+t_num es_singular_n(t_num &n, t_num &total)
+{
+    t_num msd = digito_mas_significativo_n(n);
+    if (n > 10 && msd != n % 10)
+    {
+        total += ((n / 100) + (n / 10));
+        n = n / 10;
+        es_singular_n(n, total);
+    }
+    else
+    {
+        return total;
+    }
+
+    /*if(n != 0)
+	{
+		if(mas_sig != n % 10)
+		{
+				total++;
+		}
+		n--;
+		es_singular_n(n, total, mas_sig);
+	}*/
+    return total;
+}
+
+t_num num_singulares_menoresque(t_num n)
+{
+    // Punto de entrada al algoritmo.
+    // A implementar
+    t_num num = 1;
+    t_num m = n;
+    t_num msd = digito_mas_significativo_n(n);
+
+    es_singular_n(m, num);
+    if (n == 0)
+    {
+        return 1;
+    }
+    return n + 1 - num;
+}
+
+/*
+Determina justificadamente la complejidad del algoritmo
+
+(1) Determinaci�n de las ecuaciones de recurrencia para la generalizaci�n
+
+(2) Resoluci�n utilizando los patrones vistos en clase
+
+(3) Determinaci�n justificada de la complejidad del algoritmo final.
+
+
+*/
+
+#ifndef DOM_JUDGE
+unsigned short digito_mas_significativo(t_num n)
+{
+    while (n > 9)
+    {
+        n = n / 10;
+    }
+    return (unsigned short)n;
+}
+bool es_singular(t_num n)
+{
+    unsigned short msd = digito_mas_significativo(n);
+    bool loes = true;
+    while (n > 9 && loes)
+    {
+        loes = (n % 10 != msd);
+        n = n / 10;
+    }
+    return loes;
+}
+
+t_num num_singulares_menoresque_naif(t_num n)
+{
+    t_num num = 0;
+    for (t_num i = 0; i < n; i++)
+    {
+        if (es_singular(i))
+        {
+            num++;
+        }
+    }
+    return num;
+}
+#endif
+
+#ifdef DOM_JUDGE
+bool ejecuta_caso()
+{
+    long long n;
+    cin >> n;
+    if (n == -1)
+    {
+        return false;
+    }
+    else
+    {
+        cout << num_singulares_menoresque(n) << endl;
+        return true;
+    }
+}
+#endif
+
+int main()
+{
+
+#ifndef DOM_JUDGE
+    srand(time(NULL));
+    for (int i = 1; i < 1000; i++)
+    {
+        t_num n = (t_num)rand();
+        if (num_singulares_menoresque_naif(n) != num_singulares_menoresque(n))
+        {
+            cout << "NO FUNCIONA" << endl;
+            cout << n << ":" << num_singulares_menoresque_naif(n) << "..." << num_singulares_menoresque(n) << endl;
+            break;
+        }
+    }
+    cout << "OK" << endl;
+    system("pause");
+#endif
+
+#ifdef DOM_JUDGE
+    while (ejecuta_caso())
+        ;
+#endif
+}
