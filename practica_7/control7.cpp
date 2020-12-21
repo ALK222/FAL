2f016f62d339d398a04f34f58b59e7f24f2468d7
A.L.K <alejandrobarrachina.02@gmail.com>
2020-12-21 13:30:57 +0100

    control 7 done

--- practica_7/control7.cpp
+++ practica_7/control7.cpp
@@ -14,6 +14,7 @@ INDICARLO AQUI, EXPLICANDO TAMBI�N EL MOTIVO:
 
 using namespace std;
 
+
 /*
 (1) En caso de utilizar una generalizacion,
 determinaci�n de los par�metros de la generalizaci�n.
@@ -39,105 +40,67 @@ a partir de los devueltos por la generalizaci�n.
 (4) An�lisis de la complejidad del algoritmo
 
 */
-
-bool cruza(int enemigos[], int n, int max_duelos, int power)
-{
-    int i = 0, salt = 0, suma = 0;
-    while (i < n && (enemigos[i] <= power) && (salt <= max_duelos))
-    {
-        suma += enemigos[i];
-        if (suma > power)
-        {
-            salt++;
-            suma = enemigos[i];
-        }
-        i++;
-    }
-    return i == n;
+bool cruza(int enemigos[], int n, int max_duelos, int power) {
+	int  i = 0, salt = 1, suma = 0;
+	while (i < n && (enemigos[i] <= power) && (salt <= max_duelos)) {
+		suma += enemigos[i];
+		if (suma > power) {
+			salt++;
+			suma = enemigos[i];
+		}
+		i++;
+	}
+	return  i == n && salt <= max_duelos;
 }
 
-void acota(int enemigos[], int n, int max_duelos, int &ini, int &fin)
-{
-    ini = 0;
-    fin = 2;
-    while (!cruza(enemigos, n, max_duelos, fin))
-    {
-        ini = fin;
-        fin *= 2;
-    }
+void acota(int enemigos[], int n, int max_duelos, int& ini, int& fin) {
+	ini = 0;
+	fin = 2;
+	while (!cruza(enemigos, n, max_duelos, fin)) {
+		ini = fin;
+		fin *= 2;
+	}
 }
 
-int aux(int enemigos[], int n, int max_duelos, int ini, int fin)
-{
-    int m = (ini + fin) / 2;
-    if (cruza(enemigos, n, max_duelos, m))
-    {
-        if (!cruza(enemigos, n, max_duelos, m - 1))
-        {
-            return m + enemigos[sizeof(enemigos) - 1];
-        }
-        else
-        {
-            return aux(enemigos, n, max_duelos, ini, m - 1);
-        }
-    }
-    else
-        return aux(enemigos, n, max_duelos, m + 1, fin);
+int aux(int enemigos[], int n, int max_duelos, int ini, int fin) {
+	int m = (ini + fin) / 2;
+	if (cruza(enemigos, n, max_duelos, m)) {
+		if (!cruza(enemigos, n, max_duelos, m - 1)) {
+			return m;
+		}
+		else {
+			return aux(enemigos, n, max_duelos, ini, m - 1);
+		}
+	}
+	else return aux(enemigos, n, max_duelos, m + 1, fin);
 }
 
-//int calcula_min(int enemigos[], int n, int ini, int final)
-//{
-//		int mid = ini + final / 2;
-//		if (se_vence(enemigos, n, mid))
-//		{
-//			if(!se_vence(enemigos, n,mid-1))
-//			{
-//				return mid;
-//			}
-//			else
-//			{
-//				return calcula_min(enemigos, n, ini, mid - 1);
-//			}
-//		}
-//		else
-//		{
-//			return calcula_min(enemigos, n, mid + 1, final);
-//		}
-//}
-
-int min_poder(int enemigos[], int n, int max_duelos)
-{
-    // Punto de entrada al algoritmo: a implementar
-    int ini, fin;
-    acota(enemigos, n, max_duelos, ini, fin);
-    return aux(enemigos, n, max_duelos, ini, fin);
+int min_poder(int enemigos[], int n, int max_duelos) {
+	int ini, fin;
+	acota(enemigos, n, max_duelos, ini, fin);
+	return aux(enemigos, n, max_duelos, ini, fin);
 }
+
 
 const int MAX_ENEMIGOS = 1000;
-bool ejecuta_caso()
-{
-    int enemigos[MAX_ENEMIGOS];
-    int num_enemigos;
-    cin >> num_enemigos;
-    if (num_enemigos == -1)
-    {
-        return false;
-    }
-    else
-    {
-        int max_torneos;
-        cin >> max_torneos;
-        for (int i = 0; i < num_enemigos; i++)
-        {
-            cin >> enemigos[i];
-        }
-        cout << min_poder(enemigos, num_enemigos, max_torneos) << endl;
-        return true;
-    }
+bool ejecuta_caso() {
+	int enemigos[MAX_ENEMIGOS];
+	int num_enemigos;
+	cin >> num_enemigos;
+	if (num_enemigos == -1) {
+		return false;
+	}
+	else {
+		int max_torneos;
+		cin >> max_torneos;
+		for (int i = 0; i < num_enemigos; i++) {
+			cin >> enemigos[i];
+		}
+		cout << min_poder(enemigos, num_enemigos, max_torneos) << endl;
+		return true;
+	}
 }
 
-int main()
-{
-    while (ejecuta_caso())
-        ;
+int main() {
+	while (ejecuta_caso());
 }
